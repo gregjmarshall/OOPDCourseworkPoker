2820c43d36ba7926c2f2819f7e3c30ee05439175
greg <marshall_gj@hotmail.com>
2013-03-29 10:55:52 +0000

    removed commented out old code

--- OODPCourseworkPoker/test/oodpcourseworkpoker/DealerPlayerTest.java
+++ OODPCourseworkPoker/test/oodpcourseworkpoker/DealerPlayerTest.java
@@ -262,55 +262,5 @@ public class DealerPlayerTest {
 }
         
         
-//        Player instance = new DealerPlayer(); //no longer contain those cards
-//        Card aceH = new CardImpl(Rank.ACE, Suit.HEARTS);
-//        Card aceS = new CardImpl(Rank.ACE, Suit.SPADES);
-//        Card jackS = new CardImpl(Rank.JACK, Suit.SPADES);
-//        Card nineC = new CardImpl(Rank.NINE, Suit.CLUBS);
-//        Card twoD = new CardImpl(Rank.TWO, Suit.DIAMONDS);
-//        instance.receiveCard(aceS);
-//        instance.receiveCard(aceH);
-//        instance.receiveCard(jackS);
-//        instance.receiveCard(nineC);
-//        instance.receiveCard(twoD);
-//        instance.getHand().evaluateHand();
-//        int actualPairDiscard = instance.chooseDiscard();
-//        int expectedPairDiscard = 3;
-//        assertEquals(actualPairDiscard, expectedPairDiscard);
-//        assertFalse(instance.getHand().getContents()[0].equals(twoD));
-//        assertFalse(instance.getHand().getContents()[1].equals(nineC));
-//        assertFalse(instance.getHand().getContents()[2].equals(jackS)); //this first section is for a one pair hand
-//        
-//        instance.getHand().clearHand();
-//        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
-//        instance.receiveCard(jackD);
-//        instance.receiveCard(aceH);
-//        instance.receiveCard(aceS);
-//        instance.receiveCard(jackS);
-//        instance.receiveCard(twoD);
-//        instance.getHand().evaluateHand();
-//        int actualTwoPairDiscard = instance.chooseDiscard();
-//        int expectedTwoPairDiscard = 1;
-//        assertEquals(actualTwoPairDiscard, expectedTwoPairDiscard);
-//        assertFalse(instance.getHand().getContents()[0].equals(twoD)); //this sections is for a two pair hand
-//        
-//        
-//        instance.getHand().clearHand();
-//        Card aceD = new CardImpl(Rank.ACE, Suit.DIAMONDS);
-//        instance.receiveCard(aceD);
-//        instance.receiveCard(aceH);
-//        instance.receiveCard(aceS);
-//        instance.receiveCard(jackS);
-//        instance.receiveCard(twoD);
-//        instance.getHand().evaluateHand();
-//        int actualTripsDiscard = instance.chooseDiscard();
-//        int expectedTripsDiscard = 2;
-//        assertEquals(actualTripsDiscard, expectedTripsDiscard);
-//        assertFalse(instance.getHand().getContents()[0].equals(twoD));
-//        assertFalse(instance.getHand().getContents()[1].equals(jackS)); //this section is for a trips hand
-//        
-        
-        
-        
-        
+
     
