9a6b2c52a7d3c0ac43f11c0b51138112803afc29
greg <marshall_gj@hotmail.com>
2013-03-29 10:37:19 +0000

    Completed chooseDiscard() testing in DealerPlayerTest

--- OODPCourseworkPoker/test/oodpcourseworkpoker/DealerPlayerTest.java
+++ OODPCourseworkPoker/test/oodpcourseworkpoker/DealerPlayerTest.java
@@ -79,7 +79,7 @@ public class DealerPlayerTest {
        assertEquals(expected, output);
     }
     
-    public void testChooseDiscardStraight() {
+    public void testChooseDiscardStraight() { //Tests that no cards are discarded ffrom a straight
         Player testPlayer = new DealerPlayer();
         Card jackC = new CardImpl(Rank.JACK, Suit.CLUBS);
         Card tenH = new CardImpl(Rank.TEN, Suit.HEARTS);
@@ -91,9 +91,215 @@ public class DealerPlayerTest {
         testPlayer.receiveCard(nineD);
         testPlayer.receiveCard(eightD);
         testPlayer.receiveCard(sevenH);
+        
+        int output = testPlayer.chooseDiscard();
+        int expected = 0;
+        assertEquals("Testing number of cards discarded from straight: ",expected, output);
+     }
+    
+    public void testChooseDiscardFlush() { //Tests that cards are correctly discarded for Straight
+        Player testPlayer = new DealerPlayer();
+        Card jackH = new CardImpl(Rank.JACK, Suit.HEARTS);
+        Card tenH = new CardImpl(Rank.TEN, Suit.HEARTS);
+        Card nineH = new CardImpl(Rank.NINE, Suit.HEARTS);
+        Card eightH = new CardImpl(Rank.EIGHT, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(tenH);
+        testPlayer.receiveCard(jackH);
+        testPlayer.receiveCard(nineH);
+        testPlayer.receiveCard(eightH);
+        testPlayer.receiveCard(fiveH);
+        
+        int output = testPlayer.chooseDiscard();
+        int expected = 0;
+        assertEquals("Testing number of cards discarded from flush: ",expected, output);
      }
     	
-    	
+     public void testChooseDiscardTrips() {  //tests that the correct number of cards are discarded from trips hand
+        Player testPlayer = new DealerPlayer();
+        Card jackH = new CardImpl(Rank.JACK, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card jackS = new CardImpl(Rank.JACK, Suit.SPADES);
+        Card eightH = new CardImpl(Rank.EIGHT, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(jackH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(jackS);
+        testPlayer.receiveCard(eightH);
+        testPlayer.receiveCard(fiveH);
+        int output = testPlayer.chooseDiscard();
+        int expected = 2;
+        assertEquals(output, expected);
+     }
+     
+      public void testChooseDiscardTripsPartTwo() { // tests that the correct cards are discarded from trips hand
+        Player testPlayer = new DealerPlayer();
+        Card jackH = new CardImpl(Rank.JACK, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card jackS = new CardImpl(Rank.JACK, Suit.SPADES);
+        Card eightH = new CardImpl(Rank.EIGHT, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(jackH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(jackS);
+        testPlayer.receiveCard(eightH);
+        testPlayer.receiveCard(fiveH);
+        testPlayer.chooseDiscard();
+        Card output1 = testPlayer.getHand().getContents()[0];
+        Card output2 = testPlayer.getHand().getContents()[1];
+        assertFalse("Checking five of hearts was discarded.", output1.equals(fiveH));
+        assertFalse("Checking eight of hearts was discarded.", output2.equals(eightH));
+     }
+     
+     public void testChooseDiscardTwoPair() {  //tests that the correct number of cards are discarded from a two pair hand
+        Player testPlayer = new DealerPlayer();
+        Card jackH = new CardImpl(Rank.JACK, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card eightS = new CardImpl(Rank.EIGHT, Suit.SPADES);
+        Card eightH = new CardImpl(Rank.EIGHT, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(jackH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(eightS);
+        testPlayer.receiveCard(eightH);
+        testPlayer.receiveCard(fiveH);
+        int output = testPlayer.chooseDiscard();
+        int expected = 1;
+        assertEquals("Checking only 1 card is discarded from two pair hand", output, expected);
+     }
+     
+     public void testChooseDiscardTwoPairPartTwo() {  //tests that the correct cards are discarded from a two pair hand
+        Player testPlayer = new DealerPlayer();
+        Card jackH = new CardImpl(Rank.JACK, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card eightS = new CardImpl(Rank.EIGHT, Suit.SPADES);
+        Card eightH = new CardImpl(Rank.EIGHT, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(jackH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(eightS);
+        testPlayer.receiveCard(eightH);
+        testPlayer.receiveCard(fiveH);
+        testPlayer.chooseDiscard();
+        Card output1 = testPlayer.getHand().getContents()[0];
+        assertFalse("Checking five of hearts was discarded.", output1.equals(fiveH));
+     }
+      
+     public void testChooseDiscardOnePair() {  //tests that the correct number of cards are discarded from a one pair hand
+        Player testPlayer = new DealerPlayer();
+        Card jackH = new CardImpl(Rank.JACK, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card eightS = new CardImpl(Rank.EIGHT, Suit.SPADES);
+        Card threeH = new CardImpl(Rank.THREE, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(jackH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(eightS);
+        testPlayer.receiveCard(threeH);
+        testPlayer.receiveCard(fiveH);
+        int output = testPlayer.chooseDiscard();
+        int expected = 3;
+        assertEquals("Checking 3 cards are discarded from one pair hand", output, expected);
+    }
+     
+     public void testChooseDiscardOnePairPartTwo() {  //tests that the correct cards are discarded from a one pair hand
+        Player testPlayer = new DealerPlayer();
+        Card jackH = new CardImpl(Rank.JACK, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card eightS = new CardImpl(Rank.EIGHT, Suit.SPADES);
+        Card threeH = new CardImpl(Rank.THREE, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(jackH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(eightS);
+        testPlayer.receiveCard(threeH);
+        testPlayer.receiveCard(fiveH);
+        testPlayer.chooseDiscard();
+        Card output1 = testPlayer.getHand().getContents()[0];
+        Card output2 = testPlayer.getHand().getContents()[1];
+        Card output3 = testPlayer.getHand().getContents()[2];
+        assertFalse("Checking three of hearts was discarded.", output1.equals(threeH));
+        assertFalse("Checking five of hearts was discarded.", output2.equals(fiveH));
+        assertFalse("Checking eight of spades was discarded.", output3.equals(eightS));
+     }
+     
+     public void testChooseDiscardUnmadeHand() {  //tests that the correct number of cards are discarded from a particular unmade hand
+        Player testPlayer = new DealerPlayer();
+        Card kingH = new CardImpl(Rank.KING, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card eightS = new CardImpl(Rank.EIGHT, Suit.SPADES);
+        Card threeH = new CardImpl(Rank.THREE, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(kingH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(eightS);
+        testPlayer.receiveCard(threeH);
+        testPlayer.receiveCard(fiveH);
+        int output = testPlayer.chooseDiscard();
+        int expected = 3;
+        assertEquals("Checking 3 cards are discarded from one pair hand", output, expected);
+     }
+     
+     public void testChooseDiscardUnmadeHandPartTwo() {  //tests that the correct cards are discarded from a particular unmade hand
+        Player testPlayer = new DealerPlayer();
+        Card kingH = new CardImpl(Rank.KING, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card eightS = new CardImpl(Rank.EIGHT, Suit.SPADES);
+        Card threeH = new CardImpl(Rank.THREE, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(kingH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(eightS);
+        testPlayer.receiveCard(threeH);
+        testPlayer.receiveCard(fiveH);
+        testPlayer.chooseDiscard();
+        Card output1 = testPlayer.getHand().getContents()[0];
+        Card output2 = testPlayer.getHand().getContents()[1];
+        Card output3 = testPlayer.getHand().getContents()[2];
+        assertFalse("Checking three of hearts was discarded.", output1.equals(threeH));
+        assertFalse("Checking five of hearts was discarded.", output2.equals(fiveH));
+        assertFalse("Checking eight of spades was discarded.", output3.equals(eightS));
+     }
+     
+     public void testChooseDiscardUnmadeHandTenPlus() {  //tests that the correct number of cards are discarded from a slightly better unmade hand
+        Player testPlayer = new DealerPlayer(); // in this case the 3rd card is ten or higher and only 2 cards should be discarded
+        Card kingH = new CardImpl(Rank.KING, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card tenS = new CardImpl(Rank.TEN, Suit.SPADES);
+        Card threeH = new CardImpl(Rank.THREE, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(kingH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(tenS);
+        testPlayer.receiveCard(threeH);
+        testPlayer.receiveCard(fiveH);
+        int output = testPlayer.chooseDiscard();
+        int expected = 2;
+        assertEquals("Checking 2 cards are discarded from one pair hand", output, expected);
+     }
+     
+     public void testChooseDiscardUnmadeHandTenPlusPartTwo() {  //tests that the correct cards are discarded from a slightly better unmade hand
+        Player testPlayer = new DealerPlayer();
+        Card kingH = new CardImpl(Rank.KING, Suit.HEARTS);
+        Card jackD = new CardImpl(Rank.JACK, Suit.DIAMONDS);
+        Card tenS = new CardImpl(Rank.TEN, Suit.SPADES);
+        Card threeH = new CardImpl(Rank.THREE, Suit.HEARTS);
+        Card fiveH = new CardImpl(Rank.FIVE, Suit.HEARTS);
+        testPlayer.receiveCard(kingH);
+        testPlayer.receiveCard(jackD);
+        testPlayer.receiveCard(tenS);
+        testPlayer.receiveCard(threeH);
+        testPlayer.receiveCard(fiveH);
+        testPlayer.chooseDiscard();
+        Card output1 = testPlayer.getHand().getContents()[0];
+        Card output2 = testPlayer.getHand().getContents()[1];
+        assertFalse("Checking three of hearts was discarded.", output1.equals(threeH));
+        assertFalse("Checking five of hearts was discarded.", output2.equals(fiveH));
+     }
+     
+}
+        
+        
 //        Player instance = new DealerPlayer(); //no longer contain those cards
 //        Card aceH = new CardImpl(Rank.ACE, Suit.HEARTS);
 //        Card aceS = new CardImpl(Rank.ACE, Suit.SPADES);
@@ -146,4 +352,3 @@ public class DealerPlayerTest {
         
         
     
-}
